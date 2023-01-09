68e0355957583927c22c67f0699235d99ac23954
Samir Kerimov <samir@pragmatech.az>
2023-01-09 20:46:05 +0400

    Javascript üçün praktiki tapşırıqlar 01-02

---
+++ Javascript/t01.js
@@ -0,0 +1,7 @@
+// Verilən 3 ədəd arasında ən böyük ədədi tapın.
+
+function findMax(a, b, c) {
+    // Sizin kodunuz
+}
+
+console.log(findMax(1, 2, 3)); // 3
---
+++ Javascript/t02.js
@@ -0,0 +1,7 @@
+// Daxil edilən argumentlərin cəmini tapan funksiya yazın.
+
+function sumOfArguments(...args) {
+    // Sizin kodunuz
+}
+
+console.log(sumOfArguments(1, 2, 3)); // 6
