a50247f797ce8ab7fb43f4efb8962ef19895a9eb
Samir Kərimov <samir@pragmatech.edu.az>
2023-01-27 20:53:25 +0400

    Python Praktika t16-t20 tapşırıqları əlavə edildi

---
+++ PythonPraktika/t16.py
@@ -0,0 +1,5 @@
+# list methodlarından olan append() metodunun alqoritmini yazın.Yəni elə metod yazın ki  append() metodunun funksiyasını yerinə yetirsin.
+# https://www.w3schools.com/python/ref_list_append.asp
+def customAppend(list,element):
+    # sizin kodunuz
+    pass
---
+++ PythonPraktika/t20.py
@@ -0,0 +1,5 @@
+# sort metodundan istifadə etmədən listin elementlərini böyük-dən kiçiyə doğru sıralayın.
+
+def customSort(list):
+    # sizin kodunuz
+    pass
